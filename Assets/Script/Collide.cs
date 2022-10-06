329d97bcaccd1d85f9cc17fe83821d071f342e58
03-Shafiq <fiq-chelseapro@hotmail.com>
2022-10-06 12:27:52 +0800

    score and timer

--- Assets/Script/Collide.cs
+++ Assets/Script/Collide.cs
@@ -6,19 +6,45 @@ using UnityEngine.SceneManagement;
 
 public class Collide : MonoBehaviour
 {
+    private float scorevalue;
+
+    public float totalcoins;
+
+    public float timeleft;
+
+    public int timeRemaining;
+
+    public Text ScoreText;
+    public Text TimerText;
+
+    private float TimerValue;
+
     int score;
     public Rigidbody PlayerRigidbody;
     public GameObject Text;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timeleft -= Time.deltaTime;
+        timeRemaining = Mathf.FloorToInt(timeleft % 60);
+        TimerText.text = "Timer : " + timeRemaining.ToString();
+        if (scorevalue == totalcoins)
+        {
+            if (timeleft <= TimerValue)
+            {
+                SceneManager.LoadScene("GameWin");
+            }
+            else if (timeleft <= 0)
+            {
+                SceneManager.LoadScene("GameLose");
+            }
+        }
     }
     void OnTriggerEnter(Collider other)
     {
@@ -36,6 +62,9 @@ public class Collide : MonoBehaviour
             SceneManager.LoadScene("GameLose");
 
 
+
         }
+
+
     }
 }
